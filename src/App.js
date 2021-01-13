0016a05fad12dd79c80fc84849a3363747eed799
mpakoupete <mawaki@mpakoupete.com>
2021-01-13 04:08:54 +0100

    5. About Section Styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+//import pages
 import AboutUs from './Pages/AboutUs';
+//import Global Style
+import GlobalStyle from './Components/GlobalStyle';
 
 const App = () => {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
