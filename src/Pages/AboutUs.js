34e37cdb0bc7a5fa6726f6d7edcd135c8a46ba26
mpakoupete <mawaki@mpakoupete.com>
2021-01-13 03:08:39 +0100

    4. Faq Section

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AboutSection from '../Components/AboutSection';
 import ServicesSection from '../Components/ServicesSection';
+import FaqSection from '../Components/FaqSection';
 
 
 const AboutUs = () => {
@@ -8,6 +9,7 @@ const AboutUs = () => {
         <div className="aboutus">
             <AboutSection />
             <ServicesSection />
+            <FaqSection />
         </div>
     );
 }
