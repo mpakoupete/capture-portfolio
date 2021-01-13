d92bfaa4e4fbff40f07985ba3ce230b15717fa5f
mpakoupete <mawaki@mpakoupete.com>
2021-01-13 02:55:48 +0100

    3. Services Section

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import AboutSection from '../Components/AboutSection';
+import ServicesSection from '../Components/ServicesSection';
 
 
 const AboutUs = () => {
     return (
         <div className="aboutus">
             <AboutSection />
+            <ServicesSection />
         </div>
-    )
+    );
 }
 
 export default AboutUs;
